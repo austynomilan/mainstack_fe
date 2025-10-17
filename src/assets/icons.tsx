c662e968f996e703bd25828f835c193945c7ae71
Augustine Odoh <odohaugustine@gmail.com>
2025-10-17 17:46:53 +0100

    icons uploaded

---
+++ src/assets/icons.tsx
@@ -0,0 +1,19 @@
+import analytics from "./analyticicon.svg";
+import appicon from "./appicon.svg";
+import hamburger from "./hamburger.svg";
+import homeicon from "./homeicon.svg";
+import notificationBell from "./notificationbell.svg";
+import textIcon from "./texticon.svg";
+import crmIcon from "./crmicon.svg";
+import brandLogo from "./brandLogo.svg";
+
+export const icons = {
+  analyticsIcon: analytics,
+  appIcon: appicon,
+  hamburgerIcon: hamburger,
+  homeIcon: homeicon,
+  notificationIcon: notificationBell,
+  textIcon: textIcon,
+  crmIcon: crmIcon,
+  brnadIcon: brandLogo,
+};
